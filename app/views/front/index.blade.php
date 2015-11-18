7f452b5e0977dc85aad7bbb998e6acf39952d3d4
MarkV28 <mark_vels@hotmail.com>
2015-11-18 20:28:47 +0100

    Edit index page to original version

--- app/views/front/index.blade.php
+++ app/views/front/index.blade.php
@@ -18,24 +18,23 @@
 <div class="row">
     <div id="ws-carousel" class="col-md-4 carousel slide">
         <div class="panel panel-default">
-            <div class="panel-heading">
-                <h2 class="panel-title">
+            <div class="panel-heading clearfix">
+                <a href="{{ route('workshops') }}" class="panel-title pull-left">
                     <span class="glyphicon glyphicon-cog pull-left">&nbsp;</span>
                     Workshops
-                    @if(ceil(count($workshops) / 3) > 1)
-                        <ol class="carousel-indicators pull-right">
-                            @for($i = 0; $i < (ceil(count($workshops) / 3)); $i++)
+                </a>
+                @if(ceil(count($workshops) / 3) > 1)
+                    <ol class="carousel-indicators pull-right">
+                        @for($i = 0; $i < (ceil(count($workshops) / 3)); $i++)
                             @if($i === 0)
-                            <li data-target="#ws-carousel" class="active" data-slide-to="{{ $i }}"></li>
+                                <li data-target="#ws-carousel" class="active" data-slide-to="{{ $i }}"></li>
                             @else
-                            <li data-target="#ws-carousel" data-slide-to="{{ $i }}"></li>
+                                <li data-target="#ws-carousel" data-slide-to="{{ $i }}"></li>
                             @endif
-                            @endfor
-                        </ol>
-                    @endif
-                </h2>
+                        @endfor
+                    </ol>
+                @endif
             </div>
-
                 <div class="carousel-inner">
                     <?php $i = 0; $first = true;?>
                     @foreach($workshops as $item)
@@ -49,15 +48,16 @@
                                 <h1 class="list-group-item-heading ellipsis">
                                     {{ link_to_route('workshops', $item->title, array($item->id, $item->webTitle)) }}
                                 </h1>
-				<img src="/uploads/dBn4N3hb06.png">
-                               <!-- <img class="pull-right" src="http://chart.googleapis.com/chart?cht=qr&chs=100&choe=UTF-8&chld=H&chl={{ url('workshops', $item->id) }}" alt="QR link"/> -->
-                                <div class="media-body screen-excerpt">
-                                    <p>
-                                        {{ $item->excerpt }}
-                                    </p>
-                                </div>
+                                <p class="index-excerpt">
+                                    {{ $item->excerpt }}
+                                </p>
+                                <p class="list-group-item-footer">
+                                    @if($item->is_featured)
+                                        <i class="glyphicon glyphicon-star"></i>
+                                    @endif
+                                    {{ $item->created_at->format('H:i, d M') }}
+                                </p>
                             </section>
-
                         @if($i === 2)
                             </div>
                             <?php $i = -1; ?>
@@ -80,26 +80,39 @@
         <div class="col-md-4">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    <h2 class="panel-title">
+                    <a href="{{ route('calls') }}" class="panel-title">
                         <span class="glyphicon glyphicon-warning-sign pull-left">&nbsp;</span>
                         Oproepen
-                    </h2>
+                    </a>
                 </div>
                 <div class="list-group">
                     @foreach($notes as $item)
-                    <section class="list-group-item">
-                        <h1 class="list-group-item-heading ellipsis">
-                            {{ link_to_route('news', $item->title, array($item->id, $item->webTitle)) }}
-                        </h1>
-			<img src="/uploads/dBn4N3hb06.png">
-                       <!-- <img class="pull-right" src="http://chart.googleapis.com/chart?cht=qr&chs=100&choe=UTF-8&chld=H&chl={{ url('news', $item->id) }}" alt="QR link"/> -->
-                        <div class="media-body screen-excerpt">
-                            <p>
+                        <section class="list-group-item">
+                            <h1 class="list-group-item-heading ellipsis">
+                                {{ link_to_route('news', $item->title, array($item->id, $item->webTitle)) }}
+                            </h1>
+                            <p class="index-excerpt">
                                 {{ $item->excerpt }}
                             </p>
-                        </div>
-                    </section>
+                            <p class="list-group-item-footer">
+                                @if($item->is_featured)
+                                    <i class="glyphicon glyphicon-star"></i>
+                                @endif
+                                {{ $item->created_at->format('H:i, d M') }}
+                            </p>
+                        </section>
                     @endforeach
+                    @if(count($notes) % 3 !== 0)
+                        <?php $i = 3 - (count($notes) % 3); ?>
+                        @for(; $i > 0; $i--)
+                            <section class="list-group-item">
+                                <h1 class="list-group-item-heading ellipsis">
+                                    &nbsp;
+                                </h1>
+                                <div class="media-body screen-excerpt"></div>
+                            </section>
+                        @endfor
+                    @endif
                 </div>
             </div>
         </div>
@@ -107,28 +120,54 @@
         <div class="col-md-4">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    <h2 class="panel-title">
+                    <a href="{{ route('news') }}" class="panel-title">
                         <span class="glyphicon glyphicon-bullhorn pull-left">&nbsp;</span>
                         Nieuws
-                    </h2>
+                    </a>
                 </div>
                 <div class="list-group">
                     @foreach($news as $item)
-                    <section class="list-group-item">
-                        <h1 class="list-group-item-heading ellipsis">
-                            {{ link_to_route('news', $item->title, array($item->id, $item->webTitle)) }}
-                        </h1>
-			<img src="/uploads/dBn4N3hb06.png">
-                       <!-- <img class="pull-right" src="http://chart.googleapis.com/chart?cht=qr&chs=100&choe=UTF-8&chld=H&chl={{ url('news', $item->id) }}" alt="QR link"/> -->
-                        <div class="media-body screen-excerpt">
-                            <p>
+                        <section class="list-group-item">
+                            <h1 class="list-group-item-heading ellipsis">
+                                {{ link_to_route('news', $item->title, array($item->id, $item->webTitle)) }}
+                            </h1>
+                            <p class="index-excerpt">
                                 {{ $item->excerpt }}
                             </p>
-                        </div>
-                    </section>
+                            <p class="list-group-item-footer">
+                                @if($item->is_featured)
+                                    <i class="glyphicon glyphicon-star"></i>
+                                @endif
+                                {{ $item->created_at->format('H:i, d M') }}
+                            </p>
+                        </section>
                     @endforeach
+                    @if(count($news) % 3 !== 0)
+                        <?php $i = 3 - (count($news) % 3); ?>
+                        @for(; $i > 0; $i--)
+                            <section class="list-group-item">
+                                <h1 class="list-group-item-heading ellipsis">
+                                    &nbsp;
+                                </h1>
+                                <div class="media-body screen-excerpt"></div>
+                            </section>
+                        @endfor
+                    @endif
                 </div>
             </div>
         </div>
     </div>
+@stop
+@section('script')
+    <script type="text/javascript">
+        setInterval(function(){
+            window.location.reload();
+        }, 5 * 60 * 1000);
+
+        $(document).ready(function () {
+            $('.carousel').carousel({
+                interval: 10000
+            });
+        });
+    </script>
 @stop
